1e96466d8c655ef3048d801d88a09da01a74aec7
Kieran Lynch <k.lynch@replyltd.co.uk>
2023-09-15 17:26:06 +0100

    deferring video image load and using astro image component for slider images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nikolay-zaelenchits.github.io',
-    // base: '/enso',
-    base: '/',
-
-})
+  site: "https://nikolay-zaelenchits.github.io",
+  // base: '/enso',
+  base: "/",
+  experimental: { assets: true },
+});
