885f996e91f3119d071f32500d9a39d0e569eca0
NikolayZ <n.zaelenchits@reply.com>
2023-09-15 18:28:31 +0300

    ui changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nikolay-zaelenchits.github.io',
-    // base: '/enso',
-    base: '/',
+    base: '/enso',
+    // base: '/',
 
 })
